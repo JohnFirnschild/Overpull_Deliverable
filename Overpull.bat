581b397614a3c0af22d93e4dcbd3dad86a491c5d
John Firnschild <john.firnschild@lumen.com>
2023-11-22 12:18:55 -0700

    moved batch file to repo.

---
+++ Overpull.bat
@@ -0,0 +1,3 @@
+echo DO NOT close this window
+"c:\program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" \\nadnp1b_cifs.corp.intranet\NNSGIS3\John_Firnschild\git\Overpull_Deliverable\overpull_extraction_tk.py
+exit
