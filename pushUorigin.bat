aff9078abf38572b3a68a879e5cf1770deadcabf
John Firnschild <john.firnschild@lumen.com>
2024-02-09 08:13:45 -0700

    another batch file that should have existed...

--- pushUorigin.bat
+++ pushUorigin.bat
@@ -1,36 +1,32 @@
-@echo off
-setlocal
+:: This script pushes the current branch to the remote repository and sets up tracking.
+:: If the branch does not exist on the remote repository, it is created and tracking is set up.
+:: If the branch already exists on the remote repository, it is pushed and tracking is set up.
+:: If the push fails, the script exits with an error code.
+:: Author: John Firnschild
+:: Date: 2023-10-03
+:: Version: 1.0.3
+:: Have a nice day!
 
-:: Set the name of your remote (usually "origin" by default)
-set remote=origin
+:: Set the remote repository
+set "remote=origin"
 
-:: Get the name of the current branch
-for /f "tokens=*" %%a in ('git symbolic-ref --short HEAD') do set branch=%%a
+:: Get the current branch name
+for /f "delims=" %%i in ('git rev-parse --abbrev-ref HEAD') do set "branch=%%i"
 
 :: Check if the branch exists on the remote repository
 git ls-remote --exit-code %remote% refs/heads/%branch%
 
 if %errorlevel% neq 0 (
     echo Branch "%branch%" does not exist on remote "%remote%".
-    :: Create the branch on the remote repository and set up tracking
-    git push -u %remote% %branch%
-    :: Check if the push was successful
-    if %errorlevel% equ 0 (
-        echo Branch "%branch%" has been created on remote "%remote%" and set up tracking.
-    ) else (
-        echo Failed to create branch "%branch%" on remote "%remote%".
-        exit /b 1
-    )
-) else (
-    :: Push the current branch to the remote repository and set up tracking
-    git push -u %remote% %branch%
-    :: Check the exit code of the git push command
-    if %errorlevel% equ 0 (
-        echo Successfully pushed branch "%branch%" to "%remote%" and set up tracking.
-    ) else (
-        echo Failed to push branch "%branch%" to "%remote%".
-        exit /b 1
-    )
 )
 
-endlocal
+:: Push the current branch to the remote repository and set up tracking
+git push -u %remote% %branch%
+
+:: Check the exit code of the git push command
+if %errorlevel% equ 0 (
+    echo Successfully pushed branch "%branch%" to "%remote%" and set up tracking.
+) else (
+    echo Failed to push branch "%branch%" to "%remote%".
+    exit /b 1
+)
